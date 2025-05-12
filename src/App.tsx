f1d8207ac7b80a8d3e9761426efcdb6e3cd83624
Seyrit <seyrit16@mail.ru>
2025-05-12 21:14:37 +0300

    create

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.scss'
+import * as React from "react";
+
+const App: React.FC = () =>{
+
+
+    return(
+        <div className={"app"}>
+            <h1 className={"h1-test"}>
+                Market
+            </h1>
+            <h1 className={"h2-test"}>
+                Market
+            </h1>
+            <h1 className={"h1-test"}>
+                Лохнутый лох пишет фронт
+            </h1>
+
+        </div>
+    )
+}
+
+export default App
